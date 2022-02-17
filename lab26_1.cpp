c5ef35b4f8f49c6087ec66b04d71f691b42c1a1d
M <thanaporn_chan@cmu.ac.th>
2022-02-17 13:14:57 +0700

    26 complete

--- lab26_1.cpp
+++ lab26_1.cpp
@@ -1,61 +1,73 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-
-class Node{
-    public:
-    	int data;
-    	Node *next;
-    	~Node();
+class Node
+{
+public:
+	int data;
+	Node *next;
+	~Node();
 };
 
-class List{
-	public:
-		Node *root;
-		int size;
-		void show();
-		void append(int);
-		void insert(int,int);
-		void remove(int);
+class List
+{
+public:
+	Node *root;
+	int size;
+	void show();
+	void append(int);
+	void insert(int, int);
+	void remove(int);
 };
 
-Node::~Node(){
-    cout << data << " was deleted.\n";
+Node::~Node()
+{
+	cout << data << " was deleted.\n";
 }
 
-void List::insert(int d,int idx){	
+void List::insert(int d, int idx)
+{
 	Node *n = new Node;
 	n->data = d;
-	
-	if(idx == 0){
+
+	if (idx == 0)
+	{
 		n->next = root;
 		root = n;
 		return;
 	}
 	Node *current = root;
-	for(int i = 0; i < idx-1;i++){
+	for (int i = 0; i < idx - 1; i++)
+	{
 		current = current->next;
 	}
 	n->next = current->next;
-	current->next = n;		
+	current->next = n;
 }
 
-void List::show(){
+void List::show()
+{
 	Node *current = root;
-	cout << current->data << " ";	
-	while(current->next){
+	cout << current->data << " ";
+	while (current->next)
+	{
 		current = current->next;
 		cout << current->data << " ";
-	}	
+	}
 }
 
-void List::append(int d){	
+void List::append(int d)
+{
 	Node *n = new Node;
-	n->data = d; n->next = NULL;
-	if(root == NULL) root = n;
-	else{
+	n->data = d;
+	n->next = NULL;
+	if (root == NULL)
+		root = n;
+	else
+	{
 		Node *current = root;
-		while(current->next){
+		while (current->next)
+		{
 			current = current->next;
 		}
 		current->next = n;
@@ -63,4 +75,42 @@ void List::append(int d){
 	size++;
 }
 
-//Write List::remove() here
+void List::remove(int idx)
+{
+	Node *current1 = root;
+	Node *current2 = root;
+	Node *temp1, *temp2;
+
+	for (int i = 0; i < idx; i++)
+	{
+		current2 = current2->next;
+	}
+	temp2 = current2;
+
+	for (int i = 0; i < size; i++)
+	{
+		if(i != idx - 1)
+		{
+			current1 = current1->next;
+		}
+		else
+		{
+			temp1 = current1->next;
+			current1->next = temp2->next;
+		}
+		
+		if (idx == 0)
+		{
+			delete root;
+			root = current1;
+			return;
+		}
+		else if (i == idx - 1)
+		{
+			delete temp1;
+			return;
+		}
+	}
+
+	size--;
+}
--- lab26_3.cpp
+++ lab26_3.cpp
@@ -1,14 +1,39 @@
-#include<iostream>
+#include <iostream>
+#include <set>
 
 using namespace std;
 
-//Write function count() here.
+int count(int data[], int idx)
+{
+	int count = 0, same = 0;
+	set<int> mySet;
+	for (int i = 0; i < idx; i++)
+	{
+		same = 0;
+		for (int j = i; j < idx; j++)
+		{
+			if(data[j] == data[i])
+			{
+				same++;
+			}
+		}
 
-int main(){	
-	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
-	
-	cout << "There are " << count(data,sizeof(data)/sizeof(int)); 
+		if(same == 1)
+		{
+			mySet.insert(data[i]);
+			count++;
+		}
+	}
+
+	return count;
+}
+
+int main()
+{
+	int data[] = {1, 2, 4, 5, 4, 8, 2, 1, 2, 4, 6, 1, 4, 4, 4, 2, 1, 0, 12};
+
+	cout << "There are " << count(data, sizeof(data) / sizeof(int));
 	cout << " different numbers in data.";
-	
+
 	return 0;
 }
